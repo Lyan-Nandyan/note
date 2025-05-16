d0817e15cceedba4093f379f27c0f5cbcc9c800a
Lyan_Nandyan <lyannandyan@gmail.com>
2025-05-16 22:17:21 +0700

    konfigurasi index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
 import NoteRoute from "./routes/NoteRoute.js";
+import UserRoute from "./routes/UserRoute.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({credentials:true, origin:"http://localhost:3000"}));
 app.use(express.json());
 app.use(NoteRoute);
+app.use(UserRoute);
 
 app.listen(5000,()=> console.log('server berjalan'))
