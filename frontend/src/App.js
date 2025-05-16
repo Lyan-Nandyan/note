b1bebf1a60981e5115ddffa1361e9dd422d7049c
Lyan_Nandyan <lyannandyan@gmail.com>
2025-05-17 00:48:10 +0700

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,10 +10,10 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<NoteList/>}/>
-      <Route path="login" element={<Login/>}/>
       <Route path="add" element={<AddNote/>}/>
       <Route path="edit/:id" element={<EditNote/>}/>
       <Route path="register" element={<Register/>}/>
+      <Route path="login" element={<Login/>}/>
     </Routes>
     </BrowserRouter>
   );
