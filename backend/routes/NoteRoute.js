bda2dfa264c93b9430581a7f0771a6d7f90ac558
Lyan_Nandyan <lyannandyan@gmail.com>
2025-05-16 22:16:58 +0700

    menambah middleware kepada route note

--- backend/routes/NoteRoute.js
+++ backend/routes/NoteRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import {getNotes, getNotesById, createNotes, updateNote, deleteNote} from "../controllers/NoteController.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
 
 const router = express.Router();
 
-router.get('/notes', getNotes);
-router.get('/notes/:id', getNotesById);
-router.post('/notes',createNotes);
-router.patch('/notes/:id', updateNote);
-router.delete('/notes/:id',deleteNote);
+router.get('/notes',verifyToken, getNotes);
+router.get('/notes/:id',verifyToken, getNotesById);
+router.post('/notes',verifyToken, createNotes);
+router.patch('/notes/:id',verifyToken, updateNote);
+router.delete('/notes/:id',verifyToken, deleteNote);
 
 export default router;
